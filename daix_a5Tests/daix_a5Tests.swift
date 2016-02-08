ee1c5d3490790ccbe248992e9f88327194f11091
DPC <dpc11@qq.com>
2016-02-08 14:54:14 -0500

    Initial Commit

---
+++ daix_a5Tests/daix_a5Tests.swift
@@ -0,0 +1,36 @@
+//
+//  daix_a5Tests.swift
+//  daix_a5Tests
+//
+//  Created by DPC on 16/2/8.
+//  Copyright © 2016年 DPC. All rights reserved.
+//
+
+import XCTest
+@testable import daix_a5
+
+class daix_a5Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
